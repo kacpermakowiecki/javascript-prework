59bd7165f342cf134a004b6deee7e8582352079e
Kacper Makowiecki <makkacperr@gmail.com>
2021-11-28 20:07:53 +0100

    modified:   js code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-var computerMove;
-computerMove = 'kamień';
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+	computerMove = 'papier';
+} else if (randomNumber == '3') {
+	computerMove ='nozyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
 
-var playerMove;
-playerMove = 'papier';
-
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Mój ruch: ' + computerMove);
