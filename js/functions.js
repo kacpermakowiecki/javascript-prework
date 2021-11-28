5d1092e966e9dc070e6ec4895124848bd13428b7
Kacper Makowiecki <makkacperr@gmail.com>
2021-11-28 19:20:28 +0100

    Add js code

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+function printMessage(msg){
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+}
---
+++ js/script.js
@@ -0,0 +1,7 @@
+var computerMove;
+computerMove = 'kamień';
+
+var playerMove;
+playerMove = 'papier';
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
